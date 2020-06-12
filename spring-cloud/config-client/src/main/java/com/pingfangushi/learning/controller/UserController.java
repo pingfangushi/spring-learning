9121d56ae0373826dcebf7f3aeb1b526650186ae
SanLi <2689170096@qq.com>
2020-06-12 16:37:54 +0800

    :tada: config-client

--- spring-cloud/config-client/src/main/java/com/pingfangushi/learning/controller/UserController.java
+++ spring-cloud/config-client/src/main/java/com/pingfangushi/learning/controller/UserController.java
@@ -1,8 +1,6 @@
 package com.pingfangushi.learning.controller;
 
 import com.pingfangushi.learning.configuration.UserProperties;
-import lombok.Getter;
-import lombok.Setter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,15 +16,13 @@ import java.util.Map;
  * @author SanLi
  * Created by qinggang.zuo@gmail.com / 2689170096@qq.com on 2020/6/12 15:38
  */
-@Getter
-@Setter
 @RequestMapping(value = "/user")
 @RestController
 public class UserController implements Serializable {
 	/**
 	 * logger
 	 */
-	private Logger logger = LoggerFactory.getLogger(UserController.class);
+	private final Logger logger = LoggerFactory.getLogger(UserController.class);
 
 	/**
 	 * fetch
